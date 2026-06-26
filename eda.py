4d21b353d09c59391eb935436e903d09bb32b999
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-06-26 18:06:43 +0530

    Fix filename and update EDA

--- eda.py
+++ eda.py
@@ -11,18 +11,18 @@ print(df.head())
 print(df.shape)
 print(df.describe())
 
-# ------------------------
+
 # Missing Values
-# ------------------------
+
 plt.figure(figsize=(6,4))
 sns.heatmap(df.isnull(), cbar=False, yticklabels=False, cmap="viridis")
 plt.title("Missing Values")
 plt.savefig("reports/figures/missing_values.png")
 plt.close()
 
-# ------------------------
+
 # Histograms
-# ------------------------
+
 numeric_columns = ["Temperature","Humidity","Wind_Speed","PM25","AOD"]
 
 df[numeric_columns].hist(figsize=(12,8), bins=30)
@@ -30,9 +30,9 @@ plt.tight_layout()
 plt.savefig("reports/figures/histograms.png")
 plt.close()
 
-# ------------------------
+
 # Correlation Heatmap
-# ------------------------
+
 plt.figure(figsize=(8,6))
 sns.heatmap(df[numeric_columns].corr(),
             annot=True,
@@ -43,9 +43,9 @@ plt.title("Correlation Heatmap")
 plt.savefig("reports/figures/correlation_heatmap.png")
 plt.close()
 
-# ------------------------
+
 # Boxplots
-# ------------------------
+
 for col in numeric_columns:
 
     plt.figure(figsize=(6,4))
@@ -58,9 +58,9 @@ for col in numeric_columns:
 
     plt.close()
 
-# ------------------------
+
 # PM2.5 Distribution
-# ------------------------
+
 plt.figure(figsize=(7,5))
 
 sns.histplot(df["PM25"], bins=30, kde=True)
@@ -71,9 +71,9 @@ plt.savefig("reports/figures/pm25_distribution.png")
 
 plt.close()
 
-# ------------------------
+
 # City-wise Average PM2.5
-# ------------------------
+
 plt.figure(figsize=(12,6))
 
 city_pm = df.groupby("City")["PM25"].mean().sort_values()
